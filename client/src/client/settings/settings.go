726ffdf35546b260a408792ed9da518a046df3b2
Peter Collins <peter@drifty.com>
2014-11-30 14:33:36 -0600

    Move constants to settings and restructure

--- client/src/client/settings/settings.go
+++ client/src/client/settings/settings.go
@@ -1,4 +1,12 @@
 package settings
 
-const ServerAddress string = "http://localhost:8000"
+const ServerAddress = "http://localhost:8000"
 const ClientId = "RandomId"
+
+const MinimumWorkers int = 16
+const MaxStates int = 256
+
+const M = 12
+const K = 8
+const ShardLength = 8192
+const Size = K * ShardLength
--- client/src/client/state/state.go
+++ client/src/client/state/state.go
@@ -3,15 +3,19 @@ package state
 import (
 	"sync/atomic"
 	"time"
+
+	"client/settings"
+
+	"github.com/somethingnew2-0/go-erasure"
 )
 
-const minimumWorkers int = 16
-const maxStates int = 256
-
 type StateMachine struct {
-	ClientId string
-	states   chan State
-	workers  uint32
+	ClientId    string
+	ErasureCode *erasure.Code
+	Recovered   bool // If user already existed, recover files
+	Initialized bool // If user didn't exist, upload initial files before watching
+	states      chan State
+	workers     uint32
 }
 
 type State interface {
@@ -20,14 +24,15 @@ type State interface {
 
 func NewStateMachine(clientId string) *StateMachine {
 	return &StateMachine{
-		ClientId: clientId,
-		states:   make(chan State, maxStates),
-		workers:  0,
+		ClientId:    clientId,
+		ErasureCode: erasure.NewCode(settings.M, settings.K, settings.Size),
+		states:      make(chan State, settings.MaxStates),
+		workers:     0,
 	}
 }
 
 func (sm StateMachine) Run() {
-	for i := 0; i < minimumWorkers; i++ {
+	for i := 0; i < settings.MinimumWorkers; i++ {
 		sm.SpawnWorker()
 	}
 
@@ -52,7 +57,7 @@ func (sm StateMachine) SpawnWorker() {
 
 			// Retire any workers over the threshold, once free
 			workers := atomic.LoadUint32(&sm.workers)
-			if minimumWorkers < int(workers) {
+			if settings.MinimumWorkers < int(workers) {
 				if atomic.CompareAndSwapUint32(&sm.workers, workers, workers-1) {
 					return
 				}
