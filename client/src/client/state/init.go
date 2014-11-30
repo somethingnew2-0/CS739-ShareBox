b7f9e4e2203d228c52282fb1b5925ee50133d38e
Peter Collins <peter@drifty.com>
2014-11-30 14:11:04 -0600

    Start the watch state from the init state

--- client/src/client/state/init.go
+++ client/src/client/state/init.go
@@ -14,4 +14,6 @@ func (i Init) Run(sm *StateMachine) {
 	// Say we have 1.5 GB of space available
 	http.PostForm(fmt.Sprintf("%s/client/%s/init", settings.ServerAddress, sm.ClientId),
 		url.Values{"space": {"1610612736"}})
+
+	sm.Add(Watch{})
 }
