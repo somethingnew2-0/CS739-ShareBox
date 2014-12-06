0acdf6340e60cf3447275338bf025ca595e77319
Peter Collins <peter@drifty.com>
2014-12-06 15:44:30 -0600

    Walk file directory and watch it

--- client/src/client/state/init.go
+++ client/src/client/state/init.go
@@ -2,8 +2,11 @@ package state
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"path/filepath"
 
 	"client/settings"
 )
@@ -12,16 +15,27 @@ type Init struct{}
 
 func (i Init) Run(sm *StateMachine) {
 
-	sm.Options.LoadFromJSON()
+	sm.Options.Load()
 	if sm.Options.ClientId == "" || sm.Options.UserId == "" {
 		http.PostForm(fmt.Sprintf("%s/user/new", settings.ServerAddress), url.Values{})
+		sm.Options.Save()
 
+		err := filepath.Walk(sm.Options.Dir, func(path string, info os.FileInfo, err error) error {
+			if !info.IsDir() {
+
+			}
+			return nil
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		// TODO: Upload new files
+	} else {
+
+		// TODO: Recover files
 	}
 
 	// TODO: Connect to consul and report status
-
-	// TODO: Recover files
-	// TODO: Upload new files
 
 	// Say we have 1.5 GB of space available
 	// http.PostForm(fmt.Sprintf("%s/client/%s/init", settings.ServerAddress, sm.ClientId), url.Values{"space": {"1610612736"}})
