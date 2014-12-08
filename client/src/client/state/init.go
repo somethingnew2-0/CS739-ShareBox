81b2ce228074fb456d69329fd6db889e0d472f8b
Peter Collins <peter@drifty.com>
2014-12-08 15:32:53 -0600

    Parse new user info from server

--- client/src/client/state/init.go
+++ client/src/client/state/init.go
@@ -1,7 +1,9 @@
 package state
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -9,6 +11,7 @@ import (
 	"path/filepath"
 
 	"client/settings"
+	"client/util"
 )
 
 type Init struct{}
@@ -17,17 +20,24 @@ func (i Init) Run(sm *StateMachine) {
 
 	sm.Options.Load()
 	if sm.Options.ClientId == "" || sm.Options.UserId == "" {
-		http.PostForm(fmt.Sprintf("%s/user/new", settings.ServerAddress), url.Values{})
+		resp, err := util.Post(fmt.Sprintf("%s/user/new", settings.ServerAddress), url.Values{})
+		if err != nil {
+			log.Fatal("Couldn't connect and create new user with server", err)
+		}
+		userResp := resp["user"].(map[string]interface{})
+		sm.Options.UserId = userResp["id"].(string)
+		sm.Options.ClientId = userResp["clientId"].(string)
+
 		sm.Options.Save()
 
-		err := filepath.Walk(sm.Options.Dir, func(path string, info os.FileInfo, err error) error {
+		err = filepath.Walk(sm.Options.Dir, func(path string, info os.FileInfo, err error) error {
 			if !info.IsDir() {
 
 			}
 			return nil
 		})
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("Error when walking data directory", err)
 		}
 		// TODO: Upload new files
 	} else {
---
+++ client/src/client/util/util.go
@@ -0,0 +1,25 @@
+package util
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+func Post(address string, values url.Values) (map[string]interface{}, error) {
+	resp, err := http.PostForm(address, values)
+	if err != nil {
+		return nil, err
+	}
+	respJson, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	respObj := map[string]interface{}{}
+	err = json.Unmarshal(respJson, &respObj)
+	if err != nil {
+		return nil, err
+	}
+	return respObj, nil
+}
