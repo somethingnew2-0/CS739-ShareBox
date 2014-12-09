e52e4e8148954c75db2a9ec0ed27fe7f82de0c35
Peter Collins <peter@drifty.com>
2014-12-09 01:34:21 -0600

    Add decrypt method

--- client/src/client/state/decrypt.go
+++ client/src/client/state/decrypt.go
@@ -1,13 +1,27 @@
 package state
 
 import (
+	"io/ioutil"
+	"log"
+
 	"client/keyvalue"
 )
 
 type Decrypt struct {
-	File          *keyvalue.File
-	DecodedBlocks [][]byte
+	File       *keyvalue.File
+	Ciphertext []byte
 }
 
 func (d Decrypt) Run(sm *StateMachine) {
+	plaintext := make([]byte, len(d.Ciphertext))
+	sm.Cipher.Decrypt(plaintext, d.Ciphertext)
+
+	err := sm.Files.SetFile(d.File.Name, d.File)
+	if err != nil {
+		log.Println("Error saving metadata for recovered file: ", err)
+	}
+	err = ioutil.WriteFile(d.File.Name, plaintext, 0666)
+	if err != nil {
+		log.Println("Error saving recovered file: ", err)
+	}
 }
