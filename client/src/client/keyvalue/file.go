7c6be2d5221c5b3d7c31405c693a6f6071575adc
Peter Collins <peter@drifty.com>
2014-12-09 13:02:02 -0600

    Update set for keyvalue to remove keys

--- client/src/client/keyvalue/file.go
+++ client/src/client/keyvalue/file.go
@@ -37,15 +37,22 @@ func (kv KeyValue) GetFile(path string) (*File, error) {
 	if status != 0 {
 		return nil, errors.New("File doesn't exist in the key value store")
 	}
+	if fileJson == "" {
+		return nil, errors.New("File doesn't exist in the key value store")
+	}
 	file := &File{}
 	json.Unmarshal([]byte(fileJson), &file)
 	return file, nil
 }
 
 func (kv KeyValue) SetFile(path string, file *File) error {
-	fileJson, err := json.Marshal(file)
-	if err != nil {
-		return err
+	fileJson := []byte("")
+	if file != nil {
+		var err error
+		fileJson, err = json.Marshal(file)
+		if err != nil {
+			return err
+		}
 	}
 	status, _ := kv.Set(path, string(fileJson))
 
--- client/src/client/keyvalue/replica.go
+++ client/src/client/keyvalue/replica.go
@@ -3,30 +3,44 @@ package keyvalue
 import (
 	"encoding/json"
 	"errors"
-
-	"client/thrift/replica"
 )
+
+type Replica struct {
+	ShardHash   string `json:"shardHash"`
+	ShardOffset int32  `json:"shardOffset"`
+	ShardId     string `json:"shardId"`
+	BlockId     string `json:"blockId"`
+	FileId      string `json:"fileId"`
+	ClientId    string `json:"clientId"`
+}
 
 func InitReplicaKV() *KeyValue {
 	return Init("log/replica")
 }
 
-func (kv KeyValue) GetReplica(shardId string) (*replica.Replica, error) {
+func (kv KeyValue) GetReplica(shardId string) (*Replica, error) {
 	status, replicaJson := kv.Get(shardId)
 	if status != 0 {
 		return nil, errors.New("Replica doesn't exist in the key value store")
 	}
-	replica := &replica.Replica{}
+	if replicaJson == "" {
+		return nil, errors.New("Replica doesn't exist in the key value store")
+	}
+	replica := &Replica{}
 	json.Unmarshal([]byte(replicaJson), &replica)
 	return replica, nil
 }
 
-func (kv KeyValue) SetReplica(fileId string, replica *replica.Replica) error {
-	replicaJson, err := json.Marshal(replica)
-	if err != nil {
-		return err
+func (kv KeyValue) SetReplica(shardId string, replica *Replica) error {
+	replicaJson := []byte("")
+	if replica != nil {
+		var err error
+		replicaJson, err = json.Marshal(replica)
+		if err != nil {
+			return err
+		}
 	}
-	status, _ := kv.Set(fileId, string(replicaJson))
+	status, _ := kv.Set(shardId, string(replicaJson))
 
 	if status != 0 {
 		return errors.New("Error in setting Replica in the key value store")
